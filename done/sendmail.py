08ecbd6d51af4b4789efa44c16bb7169afbb1322
buuren <vkolesni@gmail.com>
2016-08-11 09:30:38 +0300

    Update sendmail.py

--- done/sendmail.py
+++ done/sendmail.py
@@ -1,7 +1,7 @@
 import win32com.client as win32
 outlook = win32.Dispatch('outlook.application')
 mail = outlook.CreateItem(0)
-mail.To = 'vkolesni@gmail.com'
-mail.Subject = 'Swedbank monitoring'
+mail.To = 'asda@gmail.com'
+mail.Subject = ' monitoring'
 mail.body = message
 mail.send
